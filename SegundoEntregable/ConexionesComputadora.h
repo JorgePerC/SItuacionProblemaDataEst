513d496c447ab78b362dc7959457c4006493c78e
Jorge Pérez <jorgeperez_1@live.com>
2020-10-09 09:41:55 -0500

    PrincipioSegundo entregable

---
+++ SegundoEntregable/ConexionesComputadora.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+#pragma once
+
+using namespace std;
+
+
+class ConexionesComputadora {
+
+private:
+    string IP, name;
+
+    //Vector 
+    vector <string> incommingConct;
+    vector <string> outgoingConct; 
+};
