51c80dcb16852e8720176211352c7352602fe05f
Luis Alberto Fernández <49409185+LuisFdz01@users.noreply.github.com>
2020-10-17 23:59:57 -0500

    comments

--- SegundoEntregable/Main.cpp
+++ SegundoEntregable/Main.cpp
@@ -7,26 +7,26 @@
 using namespace std;
 
 int main(int argc, char const *argv[]){
-
+//    path al csv del equipo
     string filePath = "../Files/equipo4_try.csv";
-    
+//    usar clase CSV para abrir el archivo
     CSV file = CSV(filePath);
-    
+//    usar un vector para poner la información del csv en 'details'
     vector<Details> detalles = file.read_csv_details();
-
+//    usar un objeto de ConnectionManager para manejar los datos de details
     ConnectionManager* datos = new ConnectionManager(detalles);
-
+//    usar el método fill_allConnections para registrar las conexiones
     datos -> fill_allConnections();
-
-    vector <ConexionesComputadora> resultado = datos->get_allConnections();
-
-    cout << "Tamaño: " << resultado.size() << endl;
-
+//    loop para registar la conexión de cada detail en específico
     for (auto detail : detalles){
         datos -> registerConnections(detail);
     }
 
     
+//    guardar las conexiones en resultado
+    vector <ConexionesComputadora> resultado = datos->get_allConnections();
+//    imprimir los resultados
+    cout << "Tamaño: " << resultado.size() << endl;
     for (auto res : resultado){
         cout << res << endl;
         res.show_incommingConct();
@@ -36,7 +36,7 @@ int main(int argc, char const *argv[]){
 
 
     delete datos;
-
+//    un saludo para acabar el programa
     cout << "Hola" << endl;
     return 0;
 }
